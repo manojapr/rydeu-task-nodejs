77ea5423e96caa0a2a47bae74dcdc316a657cea6
gnanachandra <vvit.20bq1a05p2@gmail.com>
2023-10-19 09:40:24 +0530

    deploy to google cloud app engine

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ require("dotenv").config();
 const errorMiddleware = require("./middleware/error.middlewares");
 const connectDB = require("./db/connect");
 const pricingRouter = require("./routes/pricing.routes");
+const { StatusCodes } = require("http-status-codes");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+  res.status(StatusCodes.OK).json({message : "Task done by V. Gnana Chandra", portfolio : "https://portfolio-gnanachandra.vercel.app/", deployedIn : "Google Cloud App Engine"})
+})
 app.use("/api/pricing", pricingRouter);
 
 const start = async () => {
